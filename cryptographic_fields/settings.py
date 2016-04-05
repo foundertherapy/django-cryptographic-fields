4464825f1c3da2feb4594172862d2212577b7070
waseem-omar <waseem.omar@gmail.com>
2016-04-05 13:55:58 +0300

    Fixed an issue in getattr for encryption key

--- cryptographic_fields/settings.py
+++ cryptographic_fields/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 
-FIELD_ENCRYPTION_KEY = getattr(settings, 'FIELD_ENCRYPTION_KEY')
+FIELD_ENCRYPTION_KEY = str(getattr(settings, 'FIELD_ENCRYPTION_KEY'))
 
 if not FIELD_ENCRYPTION_KEY:
     raise ImproperlyConfigured(
