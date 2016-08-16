80a7fe63371bc427ad234e5be6842903cecb3749
nasief <nasief304@gmail.com>
2016-08-16 18:12:55 +0300

    make version reading more generic

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'cryptography.io native python encryption library.'
     ),
     url='http://github.com/foundertherapy/django-cryptographic-fields/',
-    download_url='https://github.com/foundertherapy/django-cryptographic-fields/archive/0.3.0.tar.gz',
+    download_url='https://github.com/foundertherapy/django-cryptographic-fields/archive/' + version + '.tar.gz',
     author='Dana Spiegel',
     author_email='dana@foundertherapy.co',
     install_requires=[
